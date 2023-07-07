5a93f9f9dd2e7beba1801d79c91e61e93892e7fc
muhilan <liberalsherpa@gmail.com>
2023-07-07 13:08:32 +0530

    Scaffolded SPFX project

---
+++ src/webparts/expenseManagement/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface IExpenseManagementWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'ExpenseManagementWebPartStrings' {
+  const strings: IExpenseManagementWebPartStrings;
+  export = strings;
+}
